a0ecad5e644aa1243a5e7d2c7d217905ef2b2ea1
Aleksey <xomutof.alex176@gmail.com>
2023-12-07 17:55:05 +0300

    fixed bugs(navigation)

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -7,16 +7,16 @@ import NavigationItem from '../navigation-item/index.jsx';
 const Navigation = () => {
     return (
         <div className='nav'>
-            <NavigationItem text="Продукты" img={svgLink.item1} to="/products" ></NavigationItem>
-            <NavigationItem text="Клиенты" img={svgLink.item2} to="/clients" ></NavigationItem>
-            <NavigationItem text="Категории" img={svgLink.item3} to="/categories" ></NavigationItem>
-            <NavigationItem text="Города" img={svgLink.item4} to="/cities" ></NavigationItem>
-            <NavigationItem text="Бренды" img={svgLink.item5} to="/brands" ></NavigationItem>
-            <NavigationItem text="Протоколы" img={svgLink.item6}  to="/protocols" ></NavigationItem>
-            <NavigationItem text="Заказы" img={svgLink.item7} to="/orders" ></NavigationItem>
-            <NavigationItem text="Баннеры" img={svgLink.item8} to="/banners" ></NavigationItem>
-            <NavigationItem text="Семинары" img={svgLink.item9} to="/seminars" ></NavigationItem>
-            <NavigationItem text="Промокоды" img={svgLink.item10} to="/promo" ></NavigationItem>
+            <NavigationItem text="Продукты" img={svgLink.item1} to="/products" />
+            <NavigationItem text="Клиенты" img={svgLink.item2} to="/clients" />
+            <NavigationItem text="Категории" img={svgLink.item3} to="/categories"/>
+            <NavigationItem text="Города" img={svgLink.item4} to="/cities" />
+            <NavigationItem text="Бренды" img={svgLink.item5} to="/brands" />
+            <NavigationItem text="Протоколы" img={svgLink.item6}  to="/protocols" />
+            <NavigationItem text="Заказы" img={svgLink.item7} to="/orders" />
+            <NavigationItem text="Баннеры" img={svgLink.item8} to="/banners" />
+            <NavigationItem text="Семинары" img={svgLink.item9} to="/seminars" />
+            <NavigationItem text="Промокоды" img={svgLink.item10} to="/promo" />
         </div>
     );
 };
