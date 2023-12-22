870ee7634a1c734639d6d31320a0e0dcf94cc5f9
Aleksey <xomutof.alex176@gmail.com>
2023-12-22 12:57:18 +0300

    added level_3

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,25 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
-import Products from '../../pages/Products/components';
-import Clients from '../../pages/Clients/components';
-import Category from '../../pages/Category/components/index';
+import Products from "../../pages/Products/components";
+import Clients from "../../pages/Clients/components";
+import Category from "../../pages/Category/components/index";
+import Brands from "../../pages/Brands/components";
 
-import styles from '../content/style.module.scss';
+import styles from "../content/style.module.scss";
 
-const Content:FC = () => {
-    console.log('content');
-    
+const Content: FC = () => {
+    console.log("content");
+
     return (
         <div className={styles.content}>
             <Routes>
-                <Route path='products' element={<Products/>}/>
-                <Route path='clients' element={<Clients/>}/>
-                <Route path='category' element={<Category/>}/>
+                <Route path="products" element={<Products />} />
+                <Route path="clients" element={<Clients />} />
+                <Route path="category" element={<Category />} />
+                <Route path="brands" element={<Brands />} />
             </Routes>
         </div>
     );
-}
+};
 
 export default Content;
---
+++ src/pages/Brands/components/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./style.module.scss";
+
+const Brands = () => {
+    return (
+        <div className={styles.brands__wrapp}>
+            ыфвфыв
+        </div>
+    );
+};
+
+export default Brands;
